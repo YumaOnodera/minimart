963815bfba2f1b5ed920be24aea3bce0bfe94952
YumaOnodera <yuma.onodera0913@gmail.com>
2019-10-22 23:35:36 +0900

    マイグレーションファイル修正

--- src/database/migrations/2019_10_16_085607_rename_goods_id_to_post_id_on_posts_table.php
+++ src/database/migrations/2019_10_16_085607_rename_goods_id_to_post_id_on_posts_table.php
@@ -13,11 +13,13 @@ class RenameGoodsIdToPostIdOnPostsTable extends Migration
      */
     public function up()
     {
-        Schema::disableForeignKeyConstraints(); //外部キー制約を一時的に無効化
+        Schema::table('likes', function (Blueprint $table) {
+            $table->dropForeign(['liked_goods']);
+        });
+
         Schema::table('posts', function (Blueprint $table) {
             $table->renameColumn('goods_id', 'post_id');
         });
-        Schema::enableForeignKeyConstraints(); //外部キー制約を有効化
     }
 
     /**
@@ -27,10 +29,15 @@ class RenameGoodsIdToPostIdOnPostsTable extends Migration
      */
     public function down()
     {
-        Schema::disableForeignKeyConstraints(); //外部キー制約を一時的に無効化
         Schema::table('posts', function (Blueprint $table) {
             $table->renameColumn('post_id', 'goods_id');
         });
-        Schema::enableForeignKeyConstraints(); //外部キー制約を有効化
+
+        Schema::table('likes', function (Blueprint $table) {
+            $table->foreign('liked_goods')
+                  ->references('goods_id')
+                  ->on('goods')
+                  ->onDelete('cascade');
+        });
     }
 }
